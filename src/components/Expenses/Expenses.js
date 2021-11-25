f1506871f9d87db2cad6d7bf756eec1b71bc4fd0
Sara-bit-design <saradhingra3@gmail.com>
2021-11-25 13:19:27 +0530

    adding files

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+    <Card className="expenses">
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+    <ExpensesChart expenses={filteredExpenses} />
+    {filteredExpenses.length === 0 ? <p className="notFound">No expenses found.</p> : 
+    filteredExpenses.map((expense) => (
+    <ExpenseItem 
+    key={expense.id}
+    title={expense.title} 
+    amount ={expense.amount} 
+    date={expense.date}
+    />))}
+    
+      
+    </Card>
+    </div>
+  );
+}
+
+export default Expenses;
